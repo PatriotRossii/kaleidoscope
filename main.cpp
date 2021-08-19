2445a4c019257242f82a2f56025e527ff7e0d957
PatriotRossii <patriotrossii2019@mail.ru>
2021-08-19 16:53:20 +0500

    Implement driver

--- main.cpp
+++ main.cpp
@@ -371,3 +371,69 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
 	}
 	return nullptr;
 }
+
+//===----------------------------------------------------------------------===//
+// Top-Level parsing
+//===----------------------------------------------------------------------===//
+
+static void HandleDefinition() {
+  if (ParseDefinition()) {
+    fprintf(stderr, "Parsed a function definition.\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleExtern() {
+  if (ParseExtern()) {
+    fprintf(stderr, "Parsed an extern\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+static void HandleTopLevelExpression() {
+  // Evaluate a top-level expression into an anonymous function.
+  if (ParseTopLevelExpr()) {
+    fprintf(stderr, "Parsed a top-level expr\n");
+  } else {
+    // Skip token for error recovery.
+    getNextToken();
+  }
+}
+
+/// top ::= definition | external | expression | ';'
+static void MainLoop() {
+	while(1) {
+		printf("ready> ");
+		switch(CurTok) {
+		case tok_eof:
+			return;
+		case ';':
+			getNextToken();
+			break;
+		case tok_def:
+			HandleDefinition();
+			break;
+		case tok_extern:
+			HandleExtern();
+			break;
+		default:
+			HandleTopLevelExpression();
+			break;
+		}
+	}
+}
+
+//===----------------------------------------------------------------------===//
+// Main driver code.
+//===----------------------------------------------------------------------===//
+
+int main() {
+	printf("ready> ");
+
+	getNextToken();
+	MainLoop();
+}
